a0e70cfcd2197ff02df0df91d55a83f12033b9b9
mumeanai <mumeanai@gmail.com>
2024-12-30 16:37:53 +0100

    30-12

--- src/peliculas.py
+++ src/peliculas.py
@@ -5,13 +5,13 @@ import csv
 
 Pelicula = NamedTuple(
     "Pelicula",
-    [("fecha_estreno", date), 
-    ("titulo", str), 
-    ("director", str), 
+    [("fecha_estreno", date),
+    ("titulo", str),
+    ("director", str),
     ("generos",list[str]),
     ("duracion", int),
-    ("presupuesto", int), 
-    ("recaudacion", int), 
+    ("presupuesto", int),
+    ("recaudacion", int),
     ("reparto", list[str])
     ]
 )
@@ -21,20 +21,20 @@ Pelicula = NamedTuple(
 def parsea_lista(cadena:str)-> list[str]:
     res = []
     for trozo in cadena.split(","):     #.split()   separa la cadena, como una lista de cadenas
-        res.append(trozo.strip())       #.strip()   
+        res.append(trozo.strip())       #.strip()
     return res
 
 
 def lee_peliculas(ruta:str)-> list[Pelicula]:
     '''
-    recibe una cadena de texto con la ruta de un fichero csv, 
-    y devuelve una lista de tuplas Pelicula con la información 
-    contenida en el fichero. Utilice 
-    datetime.strptime(cadena, "%d/%m/%Y").date() para parsear 
+    recibe una cadena de texto con la ruta de un fichero csv,
+    y devuelve una lista de tuplas Pelicula con la información
+    contenida en el fichero. Utilice
+    datetime.strptime(cadena, "%d/%m/%Y").date() para parsear
     las fechas. (1 punto)
     '''
     res = []
-    
+
     with open(ruta, encoding = 'utf-8') as f:
         lector = csv.reader(f, delimiter = ';')
         next(lector)
@@ -47,17 +47,17 @@ def lee_peliculas(ruta:str)-> list[Pelicula]:
             reparto = parsea_lista(reparto)
             tupla = Pelicula(fecha_estreno, titulo, director,generos, duracion, presupuesto, recaudacion, reparto)
             res.append(tupla)
-        return res    
+        return res
 
 def peliculas_mas_ganancias(peliculas: list[Pelicula], genero:str = None)-> tuple[str,str]:
     '''
-    recibe una lista de tuplas de tipo Pelicula y una cadena de 
-    texto genero, con valor por defecto None, y devuelve el título 
+    recibe una lista de tuplas de tipo Pelicula y una cadena de
+    texto genero, con valor por defecto None, y devuelve el título
     y las ganancias de la película con mayores ganancias, de entre
-    aquellas películas que tienen entre sus géneros el genero 
+    aquellas películas que tienen entre sus géneros el genero
     indicado. Si el parámetro genero es None, se busca la película
-    con mayores ganancias, sin importar sus géneros. Las ganancias 
-    de una película se calculan como la diferencia entre la 
+    con mayores ganancias, sin importar sus géneros. Las ganancias
+    de una película se calculan como la diferencia entre la
     recaudación y el presupuesto. (1 puntos)
     '''
     res =[]
@@ -68,20 +68,20 @@ def peliculas_mas_ganancias(peliculas: list[Pelicula], genero:str = None)-> tupl
             res.append(tupla)
     maximo = max(res, key = lambda t:t[1])
     return maximo
-    
+
 
 def media_presupuesto_por_genero(peliculas:list[Pelicula]) -> dict[str, float]:
     '''
-    recibe una lista de tuplas de tipo Pelicula y devuelve un 
-    diccionario en el que las claves son los distintos géneros 
-    y los valores son la media de presupuesto de las películas 
+    recibe una lista de tuplas de tipo Pelicula y devuelve un
+    diccionario en el que las claves son los distintos géneros
+    y los valores son la media de presupuesto de las películas
     de cada género. (1,5 puntos)
     '''
     presupuestos = defaultdict(list)
     for p in peliculas:
         for g in p.generos:
             presupuestos[g].append(p.presupuesto)
-    
+
     res = {}
     for  genero, presupuesto in presupuestos.items():
         res[genero] = sum(presupuesto)/len(presupuesto)
@@ -89,12 +89,12 @@ def media_presupuesto_por_genero(peliculas:list[Pelicula]) -> dict[str, float]:
 
 def peliculas_por_actor(peliculas:list[Pelicula], año_inicial:date.year = None, año_final:date.year = None) -> dict[str,int]:
     '''
-    recibe una lista de tuplas de tipo Pelicula y dos enteros año_inicial 
-    y año_final, con valor por defecto None, y devuelve un diccionario en el 
-    que las claves son los nombres de los actores y actrices, y los valores 
-    son el número de películas, estrenadas entre año_inicial y año_final 
+    recibe una lista de tuplas de tipo Pelicula y dos enteros año_inicial
+    y año_final, con valor por defecto None, y devuelve un diccionario en el
+    que las claves son los nombres de los actores y actrices, y los valores
+    son el número de películas, estrenadas entre año_inicial y año_final
     (ambos incluidos), en que ha participado cada actor o actriz. Si año_
-    inicial o año_final son None, se contarán las películas sin filtrar por 
+    inicial o año_final son None, se contarán las películas sin filtrar por
     año inicial o final, respectivamente. (1,5 puntos)
     '''
 
@@ -103,50 +103,79 @@ def peliculas_por_actor(peliculas:list[Pelicula], año_inicial:date.year = None,
         if (año_inicial == None or año_inicial <= p.fecha_estreno.year) and (año_final == None or p.fecha_estreno.year<=año_final):
             for actor in p.reparto:
                 res[actor]+=1
-    
+
     return res
 
-def actores_mas_frecuentes():
+def actores_mas_frecuentes(peliculas:list[Pelicula], n:int, año_inicial:date.year = None, año_final:date.year = None):
     '''
-    recibe una lista de tuplas de tipo Pelicula, un entero n y dos enteros 
-    año_inicial y año_final, con valor por defecto None, y devuelve una lista 
-    con los n actores o actrices que han participado en más películas estrenadas 
+    recibe una lista de tuplas de tipo Pelicula, un entero n y dos enteros
+    año_inicial y año_final, con valor por defecto None, y devuelve una lista
+    con los n actores o actrices que han participado en más películas estrenadas
     entre año_inicial y año_final (ambos incluidos). La lista de actores o actrices
-    debe estar ordenada alfabéticamente. Si año_inicial o año_final son None, se 
-    contarán las películas sin filtrar por año inicial o final, respectivamente. 
-    Haga uso de la función peliculas_por_actor para implementar esta función. 
+    debe estar ordenada alfabéticamente. Si año_inicial o año_final son None, se
+    contarán las películas sin filtrar por año inicial o final, respectivamente.
+    Haga uso de la función peliculas_por_actor para implementar esta función.
     (1 punto)
     '''
+    # res = defaultdict(list)
+    # for p in peliculas:
+    #     if (año_inicial==None or año_inicial<p.fecha_estreno.year) and (año_final==None or p.fecha_estreno.year<año_final):
+    #         for r in p.reparto:
+    #             res[r].append(p)
+    # actores_peliculas=sorted(res.items(), key=lambda t:len(t[1]))[:n]
+    # actores = [actor for actor, pelicula in actores_peliculas]
+    # return actores
     
-    pass
+    res = peliculas_por_actor(peliculas, año_inicial, año_final)
+    ordenada = sorted(res.items(), key= lambda r:r[1], reverse= True)[:n]
+    return sorted([actor for actor, num_peliculas in ordenada])
+    
+    
+            
 
-def recaudacion_total_por_año():
+
+
+
+def recaudacion_total_por_año(peliculas:list[Pelicula], generos:set[str] = None) -> dict[date.year:int]:
     '''
-    recibe una lista de tuplas de tipo Pelicula y un conjunto de cadenas de texto 
-    generos, con valor por defecto None, y devuelve un diccionario en el que las 
-    claves son los años en los que se han estrenado películas, y los valores son 
+    recibe una lista de tuplas de tipo Pelicula y un conjunto de cadenas de texto
+    generos, con valor por defecto None, y devuelve un diccionario en el que las
+    claves son los años en los que se han estrenado películas, y los valores son
     la recaudación total de las películas estrenadas en cada año que son de alguno
-    de los géneros contenidos en el parámetro generos. Si generos es None, se 
-    calcularán las recaudaciones totales de todas las películas de cada año, 
-    independientemente de su género. NOTA: Puede usar operaciones entre conjuntos 
-    para ver si existe alguna coincidencia entre los géneros de cada película y 
+    de los géneros contenidos en el parámetro generos. Si generos es None, se
+    calcularán las recaudaciones totales de todas las películas de cada año,
+    independientemente de su género. NOTA: Puede usar operaciones entre conjuntos
+    para ver si existe alguna coincidencia entre los géneros de cada película y
     los géneros especificados por el parámetro. (1,5 puntos)
     '''
-    pass
+    res = defaultdict(int)
+    for p in peliculas:
+        if generos == None:
+            res[p.fecha_estreno.year] += p.recaudacion
+        elif  set(p.generos) & generos:
+            res[p.fecha_estreno.year] += p.recaudacion   
+            
+    return res
+    
 
-def incrementos_recaudacion_por_año():
+def incrementos_recaudacion_por_año(peliculas:list[Pelicula], generos:set[str] |None = None) -> list[int]:
     '''
-    recibe una lista de tuplas de tipo Pelicula y un conjunto de cadenas de 
-    texto generos, con valor por defecto None, y devuelve una lista de enteros 
-    con las diferencias de recaudación total de cada año con respecto al anterior 
-    registrado, de películas de alguno de los géneros indicados por el parámetro 
-    generos. Si generos es None, se usarán para el cálculo las recaudaciones 
-    totales de todas las películas de cada año, independientemente de su género. 
+    recibe una lista de tuplas de tipo Pelicula y un conjunto de cadenas de
+    texto generos, con valor por defecto None, y devuelve una lista de enteros
+    con las diferencias de recaudación total de cada año con respecto al anterior
+    registrado, de películas de alguno de los géneros indicados por el parámetro
+    generos. Si generos es None, se usarán para el cálculo las recaudaciones
+    totales de todas las películas de cada año, independientemente de su género.
     Haga uso de la función recaudacion_total_por_año para implementar esta función.
     (1,5 punto)
     '''
-    pass
+    res = recaudacion_total_por_año(peliculas, generos)
+    ordenado_años = sorted(res.items())
+    diferencias = []
+    for e1, e2 in zip(ordenado_años, ordenado_años[1:]):
+        diferencias.append(e2[1]-e1[1])
+    return diferencias
 
-#Pruebe las funciones implementadas en un módulo peliculas_test.py. 
-# Se recomienda que lo vaya haciendo a medida que vaya resolviendo 
+#Pruebe las funciones implementadas en un módulo peliculas_test.py.
+# Se recomienda que lo vaya haciendo a medida que vaya resolviendo
 # los distintos apartados. (1 punto)
--- src/test_peliculas.py
+++ src/test_peliculas.py
@@ -6,25 +6,51 @@ def test_lee_peliculas(datos):
     print("Total de registros leidos:", len(datos))
     print("Mostrando los tres primeros registros:", datos[:3])
     print("Mostrando los tres últimos registros:", datos[-4:])
+    print("##########"*10)
     
 def test_peliculas_mas_ganancias(datos):
     print("Test de peliculas_mas_ganancias (genero = None): ", peliculas_mas_ganancias(datos, None))
     print("Test de peliculas_mas_ganancias (genero = Drama): ", peliculas_mas_ganancias(datos, "Drama"))
+    print("##########"*10)
 
 def test_media_presupuesto_por_genero(datos):
     print("Test de media_presupuesto_por_genero: ", media_presupuesto_por_genero(datos))
+    print("##########"*10)
 
+def imprimir_solo_tres(diccionario):
+    for actor, pelicula in  diccionario.items():
+        if actor == 'Robert Downey Jr.' or actor == 'Christian Bale' or actor == 'Adam Driver': 
+            print(f'{actor}: {pelicula}')
+        
 
 def test_peliculas_por_actor(datos):
-    print("Test de peliculas_por_actor (año_inicial =None, año_final =None", peliculas_por_actor(datos, None, None))
-    print("Test de peliculas_por_actor (año_inicial =2010, año_final =2020", peliculas_por_actor(datos, 2010, 2020))
-    print("Test de peliculas_por_actor (año_inicial =2005, año_final =2015", peliculas_por_actor(datos, 2005, 2015))
-    #falta ver que solo salgan 3 actores, y no todos 
-    
-    
+    print("Test de peliculas_por_actor (año_inicial =None, año_final =None)")
+    imprimir_solo_tres(peliculas_por_actor(datos, None, None))
+    print("Test de peliculas_por_actor (año_inicial =2010, año_final =2020)")
+    imprimir_solo_tres(peliculas_por_actor(datos, 2010, 2020))
+    print("##########"*10)
+
+def test_actores_mas_frecuentes(datos):
+    print("Test de actores_mas_frecuentes (n=3, año_inicial=2005, año_final=2015):", actores_mas_frecuentes(datos, 3, 2005, 2015))
+    print("##########"*10)
+
+def test_recaudacion_total_por_año(datos):
+    print("Test de recaudacion_total_por_año (generos=None): ")
+    print(recaudacion_total_por_año(datos, None))
+    print("Test de recaudacion_total_por_año (generos={'Drama', 'Acción'}): ")
+    print(recaudacion_total_por_año(datos, {'Drama', 'Acción'}))
+    print("##########"*10)
+
+def test_incrementos_recaudacion_por_año(datos):
+    print("Test de incrementos_recaudacion_por_año (generos=None):", incrementos_recaudacion_por_año(datos, None))
+    print("Test de incrementos_recaudacion_por_año (generos={'Drama', 'Acción'}):", incrementos_recaudacion_por_año(datos, {'Drama', 'Acción'}))
+
 if __name__ == '__main__':
     datos = lee_peliculas("data/peliculas.csv")
     test_lee_peliculas(datos)
     test_peliculas_mas_ganancias(datos)
     test_media_presupuesto_por_genero(datos)
     test_peliculas_por_actor(datos)
+    test_actores_mas_frecuentes(datos)
+    test_recaudacion_total_por_año(datos)
+    test_incrementos_recaudacion_por_año(datos)
